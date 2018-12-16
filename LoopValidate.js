ba346ed7e569a8656e0134cb21b1e2cd6c36de6f
jmht <linucks42@gmail.com>
2018-12-16 22:05:16 +0000

    Copy in appsscript form

--- LoopValidate.js
+++ LoopValidate.js
@@ -1,14 +1,33 @@
+function onOpen() {
+  var ui = SpreadsheetApp.getUi();
+  // Or DocumentApp or FormApp.
+  ui.createMenu('Loop Menu')
+      .addItem('Validate Data', 'validateData')
+      .addToUi();
+}
+
+function validateData() {
+  //var sheet = s.getSheetByName('Catalog')
+  var ss = SpreadsheetApp.getActive()
+  var catalog = DataFrame(ss.getRange('Catalog!A:R'));
+  var duplicates = catalog.duplicates('Sample Number');
+  if (duplicates.length) {
+    var data = catalog.select(duplicates,
+                              ['Sample Number', 'Your name and surname initial']);
+    SpreadsheetApp.getUi().alert("Got duplicates: " + data.join('\n'));
+  }
+}
+
 var DataFrame = function(range){
   this.range = range;
   // this.values = range.getValues();
-  this.data = [ ["Column1","Column2","Column3","Column4"],
-                  ["C1R1","C2R1","dup","C4R1"],
-                  ["C1R2","C2R2","C3R2","C4R2"],
-                  ["C1R3","C2R3","C3R3","C4R3"],
-                  ["C1R4","C2R4","dup","C4R4"],
-                ];
+ this.data = [ ["Column1","Column2","Column3","Column4"],
+                 ["C1R1","C2R1","dup","C4R1"],
+                 ["C1R2","C2R2","C3R2","C4R2"],
+                 ["C1R3","C2R3","C3R3","C4R3"],
+                 ["C1R4","C2R4","dup","C4R4"],
+               ];
 
-  // this.data = this.range.getValues();
   this.columns = this.data.shift(); // remove column header
   this.numColumns = this.columns.length;
   this.numRows = this.data.length
@@ -63,20 +82,7 @@ var DataFrame = function(range){
 
 } // End Class
 
+
 df = new DataFrame(null);
 // console.log(df.duplicates('Column3'));
 console.log(df.select([1, 3], ['Column1', 'Column3']));
-
-/*
-var ss = SpreadsheetApp.getActive()
-var catalog = DataFrame(ss.getRange('Catalog!A:R'));
-var duplicates = catalog.duplicates('Sample Number');
-if (duplicates.length) {
-  var data = catalog.select(duplicates,
-                            ['Sample Number', 'Your name and surname initial']);
-  console.log("Got duplicates: " + data.join('\n'));
-}
-*/
- // var cells = ss.getRange('Catalog!A:R')
- // var sheet = s.getSheetByName('Catalog')
- // SpreadsheetApp.getUi().alert('Got data ' + ss.getRange('Catalog!A:R').getValues().join("\n"));
