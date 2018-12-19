04f522a2e58a470cd7c30ef5a87a9f3e0300bc31
jmht <linucks42@gmail.com>
2018-12-19 20:22:25 +0000

    Fist draft of checking values

--- LoopValidate.js
+++ LoopValidate.js
@@ -18,29 +18,73 @@ function validateData() {
   }
 }
 
-var DataFrame = function(range){
-  this.range = range;
-  this.data = [];
+var DataFrame = function(range, key, name){
 
-  if (false) {
-    // Prune empty rows
-    var values = range.getValues();
-    var rowlen = 0;
-    for (var i=0; i < values.length; i++) {
-      rowlen = values[i].reduce(function(acc, val){return val.hasOwnProperty('length') ? acc + val.length : acc + 1}, 0);
-      if (rowlen > 0) {this.data.push(values[i])};
-    }
-  } else {
-    this.data = [ ["Column1","Column2","Column3","Column4"],
-                    ["C1R1","C2R1","dup","C4R1"],
-                    ["C1R2","C2R2","C3R2","C4R2"],
-                    ["C1R3","C2R3","C3R3","C4R3"],
-                    ["C1R4","C2R4","dup","C4R4"]];
-    }
+  this.init_me = function(range, key, name){
+      if (name === undefined) {
+        this.name = 'dataframe';
+      } else {
+        this.name = name;
+      }
+      if (key === undefined) {
+        throw new Error("Need db key!");
+      }
 
-  this.columns = this.data.shift(); // remove column header
-  this.numColumns = this.columns.length;
-  this.numRows = this.data.length
+      this.range = range;
+      this.data = [];
+      if (this.range != null) {
+        var values = range.getValues();
+      }  else {
+        if (this.name == 'd2') {
+          var values = [ ["Index","Column2","Column3","Column4"],
+                          ["I1","X2R1","dup","C4R1"],
+                          ["I2","C2R2","C3R2","C4R2"],
+                          ["I3","C2R3","C3R3","C4R3"],
+                          ["I4","X2R4","dup","C4R4"]];
+          } else {
+            var values = [ ["Index","Column2","Column3","Column4"],
+                            ["I1","C2R1","dup","C4R1"],
+                            ["I2","C2R2","C3R2","C4R2"],
+                            ["I3","C2R3","C3R3","C4R3"],
+                            ["I4","C2R4","dup","C4R4"]];
+          }
+      }
+
+      // Prune empty rows and set key
+      var rowlen = 0;
+      for (var i=0; i < values.length; i++) {
+        rowlen = values[i].reduce(function(acc, val){return val.hasOwnProperty('length') ? acc + val.length : acc + 1}, 0);
+        if (rowlen > 0) {this.data.push(values[i])};
+      }
+
+      this.columns = this.data.shift(); // remove column header
+      this.numColumns = this.columns.length;
+      this.numRows = this.data.length;
+
+      // set index - will ignore duplicates
+      var keyidx = this.column_indexes(key)[0];
+      this.index = {};
+      for (var i=0; i < this.data.length; i++) {
+        var keyv = this.data[i][keyidx];
+        this.index[keyv] = i;
+      }
+  }
+
+  /* Get the indexes of the supplied columns */
+  this.column_indexes = function(columns) {
+    if (!(columns instanceof Array)) {
+      columns = [columns];
+    }
+    var idx, column_idxs = [];
+    for (var i=0; i < columns.length; i++) {
+      idx = this.columns.indexOf(columns[i]);
+      if (idx == -1) {
+        throw new Error("Cannot find column: " + columns[i]);
+      }
+      column_idxs.push(idx);
+    }
+    return column_idxs;
+  }
 
   /* Return the indexes of any duplicate items in column colName */
   this.duplicates = function(colName) {
@@ -71,22 +115,19 @@ var DataFrame = function(range){
 
   /* Select data based on indexes of rows and column names */
   this.select = function(row_idxs, columns){
+    if (!(row_idxs instanceof Array)) {
+      row_idxs = [row_idxs];
+    }
     if (columns === undefined) {
       columns = this.columns;
+    } else if (typeof columns === 'string' || columns instanceof String) {
+      columns = [columns];
     }
-    // Determine which columns we are returning
-    var idx, column_idxs = [];
-    for (var i=0; i < columns.length; i++) {
-      idx = this.columns.indexOf(columns[i]);
-      if (idx == -1) {
-        throw new Error("Cannot find column: " + columns[i]);
-      }
-      column_idxs.push(idx);
-    }
-    return this.get_rows_and_columns(row_idxs,column_idxs);
+    return this.get_rows_and_columns(row_idxs, columns);
   } //End select
 
-  this.get_rows_and_columns = function (row_idxs, column_idxs){
+  this.get_rows_and_columns = function (row_idxs, columns){
+    var column_idxs = this.column_indexes(columns);
     var selected = [];
     for (var i=0; i < this.data.length; i++) {
       if (row_idxs.indexOf(i) == -1) {continue};
@@ -101,9 +142,46 @@ var DataFrame = function(range){
     return selected;
   } // End get_rows_and_columns
 
+  /* Find rows where identically labelled columns with the same index have different values */
+  this.column_difference = function(d2, column) {
+    var idx1, idx2, val1, val2, differences=[];
+    for (var key in this.index) {
+      if (key in d2.index) {
+        val1 = this.select(this.index[key], column)[0][0];
+        val2 = d2.select(d2.index[key], column)[0][0];
+        if (val1 != val2) {
+          differences.push([key, val1, val2]);
+        }
+      }
+    }
+    return differences;
+  }
+
+  this.init_me(range, key, name);
+
 } // End Class
 
 
-var df = new DataFrame(null);
+function column_differences(dataframes, columns){
+  var d1 = dataframes[0];
+  for (var i=1; i < dataframes.length; i++) {
+    d2 = dataframes[i];
+    for (var j=0; j < columns.length; j++) {
+      column = columns[j];
+      var differences = d1.column_difference(d2, column);
+      if (differences) {
+        for (var k=0; k < differences.length; k++) {
+          console.log("Got differences: %s:%s[%s] (%s)->(%s)" % (d1.name, d2.name, column, differences[k][0],differences[k][1]))
+        }
+      }
+    }
+    d1 = d2;
+  }
+}
+var df1 = new DataFrame(null, 'Index');
 // console.log(df.duplicates('Column3'));
-console.log(df.select([1, 3], ['Column1', 'Column3']));
+console.log(df1.select([1, 3], ['Column2', 'Column3']));
+var df2 = new DataFrame(null, 'Index', 'd2');
+
+// console.log(column_differences([df1, df2],['Column2']));
+console.log(df1.column_difference(df2, 'Column2'));
